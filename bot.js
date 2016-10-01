34f3981c4cddf9c7f7a1c05c679bb3dc729b4f92
emanuel katwik <ekatwikz@ekatwikz.tk>
2016-10-01 11:01:51 +0300

    Cleaned code bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,18 @@
-document.getElementById("topBar").innerHTML="<div>KATWITOOL [BOT]</div>",setInterval(function(){Game.ClickCookie(),Game.shimmerTypes.golden.time=Game.shimmerTypes.golden.maxTime,Game.shimmers.length&&Game.shimmers[0].pop();for(upgrade in Game.UpgradesById)Game.UpgradesById[upgrade].unlocked&&!Game.UpgradesById[upgrade].bought&&"toggle"!=Game.UpgradesById[upgrade].pool&&69!=upgrade&&Game.UpgradesById[upgrade].buy();var e=[];for(item in Game.ObjectsById)e.push(Game.ObjectsById[item].price);Game.ObjectsById[e.indexOf(Math.min.apply(Math,e))].buy()},1);
+document.getElementById("topBar").innerHTML = "<div>KATWITOOL [BOT]</div>";
+setInterval(function () {
+		Game.ClickCookie();
+		Game.shimmerTypes.golden.time = Game.shimmerTypes.golden.maxTime;
+		if (Game.shimmers.length) {
+			Game.shimmers[0].pop();
+		}
+		for (upgrade in Game.UpgradesById) {
+			if (Game.UpgradesById[upgrade].unlocked && !Game.UpgradesById[upgrade].bought && Game.UpgradesById[upgrade].pool != "toggle" && upgrade != 69) {
+				Game.UpgradesById[upgrade].buy();
+			}
+		}
+		var prices = [];
+		for (item in Game.ObjectsById) {
+			prices.push(Game.ObjectsById[item].price);
+		}
+		Game.ObjectsById[prices.indexOf(Math.min.apply(Math, prices))].buy();
+	}, 1);
