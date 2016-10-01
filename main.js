2c3a52c86cd539b5952fd173de848f083149ed32
emanuel katwik <ekatwikz@ekatwikz.tk>
2016-10-01 22:38:18 +0300

    Update main.js
    progress on autobuy options (very bugged)

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-var cookieCheat = {
+var outopts=[], outdiv, outselect, out;var cookieCheat = {
 	setup: [
-		[{className: "autoClick", name: "Click OFF", action: "javascript:cookieCheat.toggle(\"autoClick\")"}],
-		[{className: "cookieGen", name: "More GCookies OFF", action: "javascript:cookieCheat.toggle(\"cookieGen\")"}],
-		[{className: "autoPop", name: "Click GCookies OFF", action: "javascript:cookieCheat.toggle(\"autoPop\")"}],
-		[{className: "autoUpgrade", name: "Upgrade OFF", action: "javascript:cookieCheat.toggle(\"autoUpgrade\")"}],
-		[
-			{className: "autoBuy", name: "Buy OFF", action: "javascript:cookieCheat.toggle(\"autoBuy\")"}
-		],
+		[{className: "autoClick", name: "Click OFF", action: "javascript:cookieCheat.toggle('autoClick', 0)"}],
+		[{className: "cookieGen", name: "More GCookies OFF", action: "javascript:cookieCheat.toggle('cookieGen', 0)"}],
+		[{className: "autoPop", name: "Click GCookies OFF", action: "javascript:cookieCheat.toggle('autoPop', 0)"}],
+		[{className: "autoUpgrade", name: "Upgrade OFF", action: "javascript:cookieCheat.toggle('autoUpgrade', 0)"}],
+		
 		[
 			"multi", "wrinkler",
 			{className: "wrinkler", name: "Wrinkler", action: "javascript:cookieCheat.spawnWrinkler[0]()"},
 			{className: "shiny", name: "Shiny", action: "javascript:cookieCheat.spawnWrinkler[0](true)"}
-		]
+		],
+		[
+			"multi", "autobuy",
+			{className: "buyCheap", name: "Buy ch OFF", action: "javascript:cookieCheat.toggle('autoBuy', 0)"},
+			{className: "buyEfiicient", name: "Buy eff OFF", action: "javascript:cookieCheat.toggle('autoBuy', 1)"}
+		],
 	],
 	start: function () {
 		for (i in this) {
@@ -21,7 +24,7 @@ var cookieCheat = {
 		}
 		
 		document.getElementById("topBar").innerHTML = "<div>KATWITOOL</div>";
-		var outopts=[], outdiv, outselect, out;
+		//var outopts=[], outdiv, outselect, out;
 		for (item in this.setup) {
 			outdiv = document.createElement("div");
 			out = document.createElement("a");
@@ -40,66 +43,81 @@ var cookieCheat = {
 					out.className = this.setup[item][2].className;
 					out.id = this.setup[item][1];
 					outdiv.appendChild(out);
+					
 					for (i = 2; i < this.setup[item].length; i++) {
 						outopts.push("<option value=\"" + this.setup[item][i].className + "\"class=\"" + this.setup[item][i].className + "\"id=\"" + this.setup[item][i].action + "\">" + this.setup[item][i].name + "</option>");
 					}
 					outselect.innerHTML = outopts.join("");
-					outselect.style = "border:none;width:17px;height:16px;background:transparent;outline:none;color:white";
+					
 					outselect.onchange = function () {
 						var tochange = this.parentNode.firstElementChild;
 						tochange.innerHTML = document.getElementsByClassName(this.value)[0].innerHTML;
 						tochange.href = document.getElementsByClassName(this.value)[0].id;
 						tochange.className = this.value;
 					};
+					
+					outselect.style = "border:none;width:17px;height:16px;background:transparent;outline:none;color:white";
 					outdiv.appendChild(outselect);
 				} else if (this.setup[item][0] == "option") {
-					
 				}
 			}
-			outdiv.style = "height:29px; padding-bottom: 0px"
+			outdiv.style = "height:29px"
 			document.getElementById("topBar").appendChild(outdiv);
 		}
 	},
-	toggle: function (cheat) {
+	toggle: function (cheat, type) {
 		if (!this[cheat][1]) {
 			this[cheat][1] = !this[cheat][1];
-			this[cheat][2] = setInterval(this[cheat][0], 1);
-			for (item in document.getElementsByClassName(cheat)) {
+			if (this[cheat][2]) {
+				clearInterval(this[cheat][2]);
+			}
+			this[cheat][2] = setInterval(this[cheat][0][type], 1);
+			for (i = 0; i < document.getElementsByClassName(cheat); i++) {
+				console.log(item);
 				document.getElementsByClassName(cheat)[item].innerHTML = document.getElementsByClassName(cheat)[item].innerHTML.slice(0, -3) + "ON";
 			}
 		} else {
 			this[cheat][1] = !this[cheat][1];
 			clearInterval(this[cheat][2]);
-			for (item in document.getElementsByClassName(cheat)) {
+			for (i = 0; i < document.getElementsByClassName(cheat); i++) {
+				console.log(item);
 				document.getElementsByClassName(cheat)[item].innerHTML = document.getElementsByClassName(cheat)[item].innerHTML.slice(0, -2) + "OFF";
 			}
 		}
 	},
-	autoClick: [function () {
+	autoClick: [[function () {
 		Game.ClickCookie();
-	}],
-	cookieGen: [function () {
+	}]],
+	cookieGen: [[function () {
 		Game.shimmerTypes.golden.time = Game.shimmerTypes.golden.maxTime;
-	}],
-	autoPop: [function () {
+	}]],
+	autoPop: [[function () {
 		if (Game.shimmers.length) {
 			Game.shimmers[0].pop();
 		}
-	}],
-	autoUpgrade: [function () {
+	}]],
+	autoUpgrade: [[function () {
 		for (upgrade in Game.UpgradesById) {
 			if (Game.UpgradesById[upgrade].unlocked && !Game.UpgradesById[upgrade].bought && Game.UpgradesById[upgrade].pool != "toggle" && upgrade != 69) {
 				Game.UpgradesById[upgrade].buy();
 			}
 		}
-	}],
-	autoBuy: [function () {
+	}]],
+	autoBuy: [[function () {
 		var prices = [];
 		for (item in Game.ObjectsById) {
 			prices.push(Game.ObjectsById[item].price);
 		}
 		Game.ObjectsById[prices.indexOf(Math.min.apply(Math, prices))].buy();
-	}], 
+	}, function () {
+		var efficiencies = [];
+		for (item in Game.ObjectsById) {
+			efficiencies.push(Game.ObjectsById[item].price / Game.ObjectsById[2].storedCps);
+			/*or this? someone help me understand the difference
+			efficiencies.push(Game.ObjectsById[item].price / Game.ObjectsById[2].cps(ObjectsById[2]));*/
+		}
+		Game.ObjectsById[efficiencies.indexOf(Math.min.apply(Math, efficiencies))].buy();
+	}]], 
 	spawnWrinkler: [function (shiny) {
 		for (wrinkler in Game.wrinklers) {
 			if (!Game.wrinklers[wrinkler].phase) {
